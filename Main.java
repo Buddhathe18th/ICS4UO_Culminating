79a20370039dcc08329b792935dc28fc0b0e1a14
Alex Zhu <alexyifanzhu@gmail.com>
2023-05-16 14:52:26 -0400

    Added JInternalFrame setup for screens

--- Main.java
+++ Main.java
@@ -3,16 +3,46 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Main {
-    int screenNum=1;
+    static int screenNum=1;
+    JInternalFrame content = new JInternalFrame();
+    boolean run = true;
+
     public static void main(String[] args) {
         Main t = new Main();
     }
 
     public Main() {
         JFrame frame = new JFrame("Race Against Time");
+        frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.setSize(1920, 1080);
         frame.setVisible(true);
+
+        while (run){
+            
+            switch(screenNum){
+                case 1: TitleScreen t = new TitleScreen();
+                JInternalFrame test = t.test();
+                
+                frame.add(test);
+                test.setLocation(0, -15);
+            }
+            System.out.println(screenNum);
+
+            int pastFrame=screenNum;
+
+            while(pastFrame==screenNum){
+                try{
+                    wait(1);
+                }
+                catch (InterruptedException e){
+                    System.out.println("Broekn");
+                }
+                
+            }
+        }
+        
+
+        
     }
 }
--- TitleScreen.java
+++ TitleScreen.java
@@ -6,15 +6,15 @@ public class TitleScreen {
     
     Drawing draw = new Drawing();
 
-    public static void main(String[] args) {
-        TitleScreen t = new TitleScreen();
-    }
-
-    public TitleScreen() {
-        JFrame frame = new JFrame("Race Against Time");
+    public JInternalFrame test(){
+        
+        JInternalFrame frame = new JInternalFrame("",false,false,false,false);
+        frame.putClientProperty("JInternalFrame.isPalette", Boolean.TRUE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        frame.getRootPane().setWindowDecorationStyle(0);
         frame.add(draw);
+
+        
 
         KeyListener k = new KeyListener() {
             public void keyTyped(KeyEvent event) {
@@ -27,67 +27,16 @@ public class TitleScreen {
 
             public void keyPressed(KeyEvent event) {
                 if (event.getKeyCode() == event.VK_ENTER) {
-                    //nextFrame
-                    draw.repaint();
+                    Main.screenNum=2;
                 }
             }
         };
         frame.addKeyListener(k);
-
-        // Add labelled input fields to display
-        // JPanel inFieldPane = new JPanel();
-        // inFieldPane.setLayout(new GridLayout(3, 0));
-        // JPanel topTitle = new JPanel();
-        // topTitle.add(new Drawing());
-        // topTitle.add(new JLabel("LC Studios Presents..."));
-        // inFieldPane.add(topTitle);
-        // inFieldPane.add(new JLabel("Race Against Time"));
-        // inFieldPane.add(new JLabel("Press enter to continue..."));
-        // frame.add(inFieldPane);
-
-        // inFieldPane.add(new JLabel("Given Name"));
-        // inFieldPane.add(givenName);
-        // givenName.addActionListener(this);
-        // inFieldPane.add(new JLabel("Family Name"));
-        // inFieldPane.add(familyName);
-        // familyName.addActionListener(this);
-        // frame.add(inFieldPane, BorderLayout.NORTH);
-        // // Add submission button
-        // JPanel submitPane = new JPanel();
-        // submitPane.setLayout(new FlowLayout());
-        // submitPane.add(new JLabel("Press Button to Enter Names"));
-        // KeyListener k = new KeyListener() {
-        // public void keyTyped(KeyEvent event) {
-        // return;
-        // }
-
-        // public void keyReleased(KeyEvent event) {
-        // return;
-        // }
-
-        // public void keyPressed(KeyEvent event) {
-        // if (event.getKeyCode() == event.VK_ENTER) {
-        // String fullString = familyName.getText().trim() + ", "
-        // + givenName.getText().trim();
-        // fullName.setText(fullString);
-        // }
-        // }
-        // };
-        // givenName.addKeyListener(k);
-        // familyName.addKeyListener(k);
-        // submitPane.add(submitButton);
-        // frame.add(submitPane, BorderLayout.CENTER);
-
-        // // Add Output fields
-        // JPanel outFieldPane = new JPanel();
-        // outFieldPane.setLayout(new GridLayout(1, 2));
-        // outFieldPane.add(new JLabel("Full Name"));
-        // outFieldPane.add(fullName);
-        // frame.add(outFieldPane, BorderLayout.SOUTH);
-        // // Display the final product
         frame.setSize(1920, 1080);
         frame.setVisible(true);
+        return frame;
     }
+
 
     class Drawing extends Canvas {
         public void paint(Graphics g) {
