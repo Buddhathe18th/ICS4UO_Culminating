ba97822a7759f5384d3509b0abbf4a6ef9579720
Alex Zhu <alexyifanzhu@gmail.com>
2023-06-08 20:34:41 -0400

    Credit screen works

--- Main/Credits.java
+++ Main/Credits.java
@@ -36,7 +36,7 @@ innerPanel = new Panel();
 
         innerPanel.add(mainMenu);
         mainMenu.setSize(new Dimension(600,50));
-        mainMenu.setLocation(405, 375);
+        mainMenu.setLocation(305, 505);
 
         return frame;
     }
@@ -67,7 +67,14 @@ innerPanel = new Panel();
                 e.printStackTrace();
             }
 
-            g.drawImage(iArr[0], 200, 300,200,200, null);
+            g.drawImage(iArr[0], 500, 100,200,150, null);
+
+            g.setFont(new Font("Courier New", Font.PLAIN, 50));
+            g.drawString("LC Studios", 450, 330);
+            g.setFont(new Font("Courier New", Font.PLAIN, 70));
+            g.drawString("Race Against Time", 245, 430);
+            g.setFont(new Font("Courier New", Font.PLAIN, 20));
+            g.drawString("Developers: Alex Zhu, Lukas Li", 400, 490);
         }
     }
 
